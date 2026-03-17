720feb29e6b18a31feebba80c892a608860444f9
Sriram-thota <thotasriram13@gmail.com>
2026-03-17 09:28:38 +0530

    Update element locators and save method in TasksPage
    Refactor TasksPage to improve element locators and save functionality.

--- pages/tasks_page.py
+++ pages/tasks_page.py
@@ -2,40 +2,48 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
-
+import time
 
 class TasksPage(BasePage):
+    FIRST_EDIT_BUTTON = (By.XPATH, "//table//tbody//tr[not(.//span[contains(@class,'badge') and text()='done'])][1]//button[contains(.,'Edit')]")
+    STATUS_DROPDOWN   = (By.XPATH, "//label[text()='Status']/following::select[1]")
+    SAVE_BUTTON       = (By.ID, "tasks-save-btn")  # fixed - using ID
+    MODAL_BACKDROP    = (By.XPATH, "//div[contains(@class,'modal-backdrop')]")
 
-    FIRST_EDIT_BUTTON = (By.XPATH, "//table//tbody//tr[1]//button[contains(@class,'btn-outline-primary')]")
-
-    STATUS_DROPDOWN = (By.XPATH, "//label[text()='Status']/following::select[1]")
-
-    SAVE_BUTTON = (By.XPATH, "//button[contains(.,'Save')]")
-
-    STATUS_BADGE = (By.XPATH, "//table//tbody//tr[1]//span[contains(@class,'badge')]")
+    def get_first_non_done_row_badge(self):
+        return (By.XPATH, "//table//tbody//tr[not(.//span[contains(@class,'badge') and text()='done'])][1]//span[contains(@class,'badge bg-')]")
 
     def click_first_edit(self):
+        self.wait.until(
+            EC.visibility_of_element_located(self.FIRST_EDIT_BUTTON)
+        )
         self.click(self.FIRST_EDIT_BUTTON)
-
         self.wait.until(
             EC.visibility_of_element_located(self.STATUS_DROPDOWN)
         )
 
     def change_status(self, status):
+        self.wait.until(
+            EC.element_to_be_clickable(self.STATUS_DROPDOWN)
+        )
         dropdown = Select(self.find(self.STATUS_DROPDOWN))
         dropdown.select_by_value(status)
 
     def save_task(self):
-        self.click(self.SAVE_BUTTON)
-
-        # wait until table updates
-        self.wait.until(
-            EC.visibility_of_element_located(self.STATUS_BADGE)
+        save_btn = self.wait.until(
+            EC.element_to_be_clickable(self.SAVE_BUTTON)
         )
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", save_btn)
+        time.sleep(0.5)
+        # Use JS click to avoid any overlay issues
+        self.driver.execute_script("arguments[0].click();", save_btn)
+        self.wait.until(
+            EC.invisibility_of_element_located(self.MODAL_BACKDROP)
+        )
+        time.sleep(1)
 
     def get_status(self):
-        # always re-find the badge to avoid stale element
         badge = self.wait.until(
-            EC.presence_of_element_located(self.STATUS_BADGE)
+            EC.presence_of_element_located(self.get_first_non_done_row_badge())
         )
         return badge.text
