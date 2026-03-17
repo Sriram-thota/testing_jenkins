4fc4df3a4f918ea2806cc008ac9a0d16d2d054b4
Sriram-thota <thotasriram13@gmail.com>
2026-03-17 09:28:14 +0530

    Refactor login page element locators and methods
    Refactor login page locators to use IDs instead of XPATH and CSS selectors. Simplify role selection methods.

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,21 +1,18 @@
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 
-
 class LoginPage(BasePage):
-
-    USERNAME_INPUT = (By.XPATH, "//input[@placeholder='Enter email']")
-    PASSWORD_INPUT = (By.XPATH, "//input[@placeholder='Enter password']")
-    LOGIN_BUTTON = (By.CSS_SELECTOR, "button[type='submit']")
-
-    ADMIN_BUTTON = (By.XPATH, "//button[contains(.,'Admin')]")
-    USER_BUTTON = (By.XPATH, "//button[contains(.,'User')]")
-
-    def select_user_role(self):
-        self.click(self.USER_BUTTON)
+    USERNAME_INPUT = (By.ID, "login-email-input")
+    PASSWORD_INPUT = (By.ID, "login-password-input")
+    LOGIN_BUTTON = (By.ID, "login-submit-btn")
+    ADMIN_BUTTON = (By.ID, "login-fill-admin-btn")
+    USER_BUTTON = (By.ID, "login-fill-user-btn")
 
     def select_admin_role(self):
         self.click(self.ADMIN_BUTTON)
+
+    def select_user_role(self):
+        self.click(self.USER_BUTTON)
 
     def enter_username(self, username):
         self.type(self.USERNAME_INPUT, username)
@@ -26,14 +23,12 @@ class LoginPage(BasePage):
     def click_login(self):
         self.click(self.LOGIN_BUTTON)
 
-    # Admin login helper
     def login_as_admin(self, username, password):
         self.select_admin_role()
         self.enter_username(username)
         self.enter_password(password)
         self.click_login()
 
-    # User login helper
     def login_as_user(self, username, password):
         self.select_user_role()
         self.enter_username(username)
